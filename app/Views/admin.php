522970884884566abfc6092dd3f7cb8bab5c356c
leo <nickholasleonardo@gmail.com>
2023-06-27 11:28:16 +0700

    tes

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
+        //TESTING
         $(document).ready(function() {
             $("#input-button").click(function(e) {
                 e.preventDefault();
